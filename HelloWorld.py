77d3d4707245b6691afc036df14cad3b9d5ed348
Mansoor Naseer <mansoor.naseer@hotmail.com>
2017-05-02 21:57:25 +0500

    the string is moving too fast

--- HelloWorld.py
+++ HelloWorld.py
@@ -9,12 +9,15 @@ myriadProFont = pygame.font.SysFont("Myriad Pro", 48)
 helloWorld = myriadProFont.render("Hello World", 1, (255,0 ,255),(255,255,255))
 
 
-
+x,y = 0,0 # to use the position of helloworld string
 
 while 1:
     for event in pygame.event.get():
         
         if event.type == pygame.QUIT:
             sys.exit()
-    screen.blit(helloWorld,(0,0))
+    screen.blit(helloWorld,(x,y))
+    
+    x+=1
+    
     pygame.display.update()
