5399b244953fb9e828dc4c35e1f81e7386ec6a64
Mansoor Naseer <mansoor.naseer@hotmail.com>
2017-05-02 22:05:32 +0500

    Now the code ensures 40fps and the screen is cleaned on each iteration

--- HelloWorld.py
+++ HelloWorld.py
@@ -11,13 +11,20 @@ helloWorld = myriadProFont.render("Hello World", 1, (255,0 ,255),(255,255,255))
 
 x,y = 0,0 # to use the position of helloworld string
 
+clock = pygame.time.Clock()
+
 while 1:
+    
+    clock.tick(40) # it ensures us that the python will render 40 fps
+    
     for event in pygame.event.get():
         
         if event.type == pygame.QUIT:
             sys.exit()
+            
+    screen.fill((0,0,0)) # it blacks the screen on start of every loop
     screen.blit(helloWorld,(x,y))
     
-    x+=1
+    x+=5
     
     pygame.display.update()
